fdb209425f0a2ba64f401b66d80863a2d7cf92e8
mohammed337 <moiskh330@gmail.com>
2023-07-07 16:09:34 +0300

    fixing ranking

--- core/models.py
+++ core/models.py
@@ -5,7 +5,8 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     student_id = models.CharField(max_length=50)
     total_grade = models.DecimalField(max_digits=5, decimal_places=2)
-    
+    rank = models.IntegerField(default=0)
+
     def clean(self):
         if self.total_grade > 1500:
             raise ValidationError('Total grade cannot be greater than 1500')
--- core/views.py
+++ core/views.py
@@ -3,16 +3,38 @@ from django.http import JsonResponse
 from django.db.models import Q
 
 from .models import Student
+
+
 def home(request):  
     if request.GET.get('search'):
         search_term = request.GET.get('search')
         grades = Student.objects.filter(
            Q(name__icontains=search_term) | Q(student_id__contains=search_term)
-       ).order_by('-total_grade')
-    else:    
-        grades = Student.objects.all().order_by('-total_grade')
+       ).order_by('-total_grade')  
         
-    count = Student.objects.count()   
-    return render(request, 'index.html', context={'grades':grades,'count': count})
+        # Get ranks from DB    
+        ranked_grades = Student.objects.filter(
+            id__in=[g.id for g in grades]
+        ).order_by('rank').values_list('rank', flat=True)
+        
+        # Zip ranks with grades    
+        ranked_grades = zip(ranked_grades, grades)
 
-
+        context = {
+            'ranked_grades': ranked_grades  
+        }
+        
+    else:    
+        grades = Student.objects.all().order_by('-total_grade')  
+        
+        # Get ranks from DB    
+        ranked_grades = Student.objects.all().order_by('rank').values_list('rank', flat=True)
+        
+        # Zip ranks with grades    
+        ranked_grades = zip(ranked_grades, grades)
+        
+        context = {
+            'ranked_grades': ranked_grades   
+        }
+    
+    return render(request, 'index.html', context)
